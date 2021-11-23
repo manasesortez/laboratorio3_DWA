fe0c90ec43a76e62539d5a8abf1f10faaec831ec
manasesortez <manasesortez1234@hotmail.com>
2021-11-23 10:51:26 -0600

    node.js lab

---
+++ ejercicio1/ejercicio1-2/index.js
@@ -0,0 +1,6 @@
+/**
+ * Crear un programa que importe y exporte otros módulos, dichos módulos 
+ * deben contener breve información personal (la que sea que usted quiera 
+ * agregar), lógicamente debe crear una carpeta que se puede llamar 
+ * “libs” sino como usted desee; para exportarla.
+ */
